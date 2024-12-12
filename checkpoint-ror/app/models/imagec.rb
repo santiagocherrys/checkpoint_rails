58362a3ffc8b859f9cf12fd331dae54a75f35910
Santiago Echeverry <santiagocherrys@hotmail.com>
2024-12-12 09:43:46 -0500

    Added validation of type of image and traduction to that error en|es

--- checkpoint-ror/app/models/imagec.rb
+++ checkpoint-ror/app/models/imagec.rb
@@ -1,3 +1,30 @@
 class Imagec < ApplicationRecord
+
+  #this is for active storage
   has_one_attached :avatar
+
+  #validations
+  validates :name, presence: true, length: {maximum: 80}
+  validates :description, presence: true, length: {maximum: 150}
+
+  #custom validation
+  validate :image_type
+
+  private
+
+  def image_type
+    # Check if the avatar is attached
+    unless avatar.attached?
+      errors.add(:avatar, I18n.t("imagecs.missing"))
+      return # Stop further checks if no avatar is attached
+    end
+  
+    # Validate the content type if an avatar is attached
+    # for using t outside of view and controllers layer use I18n
+    unless avatar.content_type.in?(%w(image/jpeg image/png))
+      errors.add(:avatar, I18n.t("imagecs.wrong_file"))
+    end
+  end
+  
+
 end
