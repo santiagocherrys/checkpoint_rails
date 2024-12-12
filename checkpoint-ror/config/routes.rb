8eff814413c40705d0f2eee107ed8674201f847f
Santiago Echeverry <santiagocherrys@hotmail.com>
2024-12-12 08:15:29 -0500

    Added all internetionalization

--- checkpoint-ror/config/routes.rb
+++ checkpoint-ror/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :imagecs
+  scope "(:locale)", locale: /en|es|ja/ do
+    # Defines the root path route ("/")
+    root "imagecs#index"
+    resources :imagecs
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -10,6 +14,4 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
-  # Defines the root path route ("/")
-  root "imagecs#index"
 end
